018c6ca1cea235f04b41677eb0975fe93e1947ae
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-06-08 20:48:34 -0700

    added functionality to character sheet, and exit of character sheet

--- src/js/index.js
+++ src/js/index.js
@@ -2,17 +2,17 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './../css/styles.css';
-import  Pokemon  from './pokemon.js';
-import { Character } from './rpg.js';
+//import  Pokemon  from './pokemon.js';
+//import { Character } from './rpg.js';
 
-function clearFields() {
-  $('.showPoke').html("");
-}
+// function clearFields() {
+//   $('.showPoke').html("");
+// }
 
-function diceRoller() {
-  let rollDice = Math.floor((Math.random() * 256) + 1);
-  return rollDice;
-}
+// function diceRoller() {
+//   let rollDice = Math.floor((Math.random() * 256) + 1);
+//   return rollDice;
+// }
 
 
 
@@ -20,19 +20,10 @@ function diceRoller() {
 
 
 $(document).ready(function() {
-  $("#randomPoke").click(function () {
-    let Bob = new Character([0, 0, 0, 0], [0, 0, 0, 0], 0, " ", 0);
-    Bob.classStart("Mother", Bob);
-    console.log(Bob.growths);
-    let number = diceRoller();
-    clearFields();
-    let promise = Pokemon.getPokemon(number);
-
-    promise.then(function(response) {
-      const body = JSON.parse(response);
-
-  
-      $('.showPoke').html( `${body.species.name}` + `<img src="${body.sprites.front_default}" id="pokemon" />` );
-    });
+  $("#sheetButton").click(function() {
+    $("#characterSheet").show();
+  });
+  $("#closeCharacterSheet").click(function() {
+    $("#characterSheet").toggle();
   });
 });
